b91b6a0da5493c6197e00693b111e64e662c1939
Cabañero <vilmar.cabanero@accenture.com>
2022-07-10 19:44:46 +0800

    Fix the bug in Title.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -22,11 +22,11 @@ export function App() {
   return (
     <BrowserRouter>
       <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
+        titleTemplate="%s - Fiona"
+        defaultTitle="Fiona"
         htmlAttributes={{ lang: i18n.language }}
       >
-        <meta name="description" content="A React Boilerplate application" />
+        <meta name="description" content="A Fiona application" />
       </Helmet>
 
       <Switch>
